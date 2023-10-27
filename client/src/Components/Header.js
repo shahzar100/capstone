1a70f21bf191d1b819b2d0c07953d3901218d0f4
Shahzar Ali <shahzarali@Shahzars-MacBook-Air.local>
2023-10-27 10:13:38 +0100

    added meta tags to index

---
+++ client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+const Header = () => {
+	return (
+		<header className="App-header">
+			<img src='./Logo.svg' alt='Logo'/>
+			<Nav/>
+		</header>
+	)
+}
+
+export default Header
---
+++ client/src/Components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+	return (
+		<nav>
+			<a> Menu </a>
+			<a>  </a>
+
+		</nav>
+	)
+}
+
+export default Nav
